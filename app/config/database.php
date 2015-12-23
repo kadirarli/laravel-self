10b98783f1df03abc04c4e51f70b27dc14b1681d
kadirarli <kadirarli@sirketing.com>
2015-12-23 19:01:36 +0200

    Some bugs are fixed

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel-self',
+			'database'  => 'laravel-self2',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
